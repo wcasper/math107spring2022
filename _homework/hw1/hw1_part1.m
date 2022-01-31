8a203d4b3725f7ec793f4a79e2d06f3b5f74da1c
wcasper <williamrileycasper@gmail.com>
2022-01-31 11:29:37 -0800

    added double checks

--- _homework/hw1/hw1_part1.m
+++ _homework/hw1/hw1_part1.m
@@ -3,12 +3,15 @@
 
 %% Problem 2
 % define your variables x,y,z,w, and q below
+% DOUBLE CHECK: your value of w should be approx. 11.5697
+% DOUBLE CHECK: your value of q should be approx. 360.8594
 
 %% Problem 3
 
 % define variable T_room and set it to the room temperature
 
 % define variable T_0 and set it to the temperature of a living human
+% DOUBLE CHECK: your value of T_0 should be 37
 
 % define a variable r and set it equal to the specific heat capacitance
 
@@ -16,5 +19,6 @@
 
 % now use the equation above to determine the time it took for the body to cool
 % make sure to use the variable t for your result!!
+% DOUBLE CHECK: your value of t should be approx. 16.8598
 
 
